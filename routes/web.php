dc4e2933d496fe482279a82c21286ebf3f757afb
tienshake <tientrancntt@gmail.com>
2024-12-25 02:28:11 +0700

    feat: redirect admin page when in root

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
