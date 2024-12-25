79a4555276f94f9b00c6492ffc176cf23edc9238
tienshake <tientrancntt@gmail.com>
2024-12-25 14:53:51 +0700

    fix: bug

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('12345678a@'),
             'remember_token' => Str::random(10),
             'role' => 'admin'
         ];
